8af9f89facc68a88118e2e0bfec865550e145ac0
Dragos Ginjoveanu <dragosginjoveanu@Dragoss-MacBook-Pro.local>
2025-02-24 12:19:21 +0200

    modified product aspect

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -3,19 +3,23 @@ import { NavLink } from "react-router-dom";
 export default function Product({ productData }) {
   return (
     <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <img
-        className="p-8 rounded-t-lg"
-        src={`/images/${productData.image}`}
-        alt="product image"
-      />
+      <div>
+        <img
+          className="p-8 rounded-t-lg object-cover w-full h-64"
+          src={`/images/${productData.image}`}
+          alt="product image"
+        />
+      </div>
       <div className="px-5 pb-5">
         <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
           {productData.title}
         </h5>
-        <div className="flex items-center justify-between">
+        <div>
           <span className="text-3xl font-bold text-gray-900 dark:text-white">
             ${productData.price}
           </span>
+        </div>
+        <div className="flex items-center justify-between">
           <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Product details
           </button>
