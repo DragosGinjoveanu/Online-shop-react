860ee4e71f41c6e97f01446788e5ce302f9f01ef
Dragos Ginjoveanu <dragosginjoveanu@Dragoss-MacBook-Pro.local>
2025-02-27 14:30:54 +0200

    added pricing filters

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,48 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
+
 import ProductsList from "./products/ProductsList";
 import SearchBar from "./Search/SearchBar";
 import CategoryFilter from "./Search/CategoryFilter";
 import StockFilter from "./Search/StockFilter";
+import PriceFilter from "./Search/PriceFilter";
+
+import { getMaxProductPrice } from "../store/helpers/products";
 
 export default function Home() {
   const products = useSelector((store) => store.products.products);
+
+  // function that gets the maximum price
+
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("");
   const [showInStockOnly, setShowInStockOnly] = useState(false);
 
-  const filteredProducts = products.filter((product) => {
-    const matchesSearch = product.title
-      .toLowerCase()
-      .startsWith(searchQuery.toLowerCase());
+  const [sortOrder, setSortOrder] = useState("");
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(getMaxProductPrice(products));
 
-    const matchesCategory =
-      selectedCategory === "" || product.category === selectedCategory;
+  const filteredProducts = products
+    .filter((product) => {
+      const matchesSearch = product.title
+        .toLowerCase()
+        .startsWith(searchQuery.toLowerCase());
 
-    const matchesStock = !showInStockOnly || product.quantity > 0;
+      const matchesCategory =
+        selectedCategory === "" || product.category === selectedCategory;
 
-    return matchesSearch && matchesCategory && matchesStock;
-  });
+      const matchesStock = !showInStockOnly || product.quantity > 0;
+
+      const matchesPrice =
+        product.price >= minPrice && product.price <= maxPrice;
+
+      return matchesSearch && matchesCategory && matchesStock && matchesPrice;
+    })
+    .sort((a, b) => {
+      if (sortOrder === "asc") return a.price - b.price;
+      if (sortOrder === "desc") return b.price - a.price;
+      return 0;
+    });
 
   return (
     <div className="flex gap-6 px-4">
@@ -35,6 +55,15 @@ export default function Home() {
         <StockFilter
           showInStockOnly={showInStockOnly}
           setShowInStockOnly={setShowInStockOnly}
+        />
+        <PriceFilter
+          maxProductPrice={getMaxProductPrice(products)}
+          sortOrder={sortOrder}
+          setSortOrder={setSortOrder}
+          minPrice={minPrice}
+          setMinPrice={setMinPrice}
+          maxPrice={maxPrice}
+          setMaxPrice={setMaxPrice}
         />
       </div>
 
